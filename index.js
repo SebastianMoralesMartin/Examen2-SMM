e8504dc63a284dad7b4d009224b2354175778868
SebastianMoralesMartin <sebfut@hotmail.com>
2021-10-21 23:25:59 -0500

    dump

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const mongoose = require('mongoose')
 const ciudadRoutes = require('./routes/ciudad')
 const app = express()
-
+//
 app.use(express.json())
 app.use(express.urlencoded({extended: true}))
 app.use('/examen2', ciudadRoutes)
